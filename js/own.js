69cbfdba6ce2375d06052b499aadd88fb01cf1b1
leo7044 <cc.ta.leo7044@gmail.com>
2017-09-11 17:34:10 +0200

    Optik Schlagwörter angepasst

--- js/own.js
+++ js/own.js
@@ -1,12 +1,42 @@
 ﻿$(document).ready(function() {
+	// Navigation zwischen den Fachbereichen
 	$('button.btn.btn-default.btn-sm.btn-block').click(changeFachbereich);
-	$("#FileInputUploadArbeit").fileinput({ // FileUploadAvatar
+	
+	// FileUploadArbeit
+	$("#FileInputUploadArbeit").fileinput({
         showUpload: false, // Zeile 698 in "fileinput.js" bearbeitet, showUpload auf false gesetzt; Zeile 3.930 in "fileinput.js" bearbeitet, um "Remove"-Button rot einzufärben
 		uploadUrl: 'upload',
 		allowedFileTypes: ['pdf'],
         overwriteInitial: true,
 		defaultPreviewContent: '<i class="glyphicon glyphicon-file" style="font-size:100px;"></i><h6 class="text-muted">Laden Sie Ihre Arbeit hier hoch (.pdf-Format), unterstützt Drag & Drop</h6>'
     });
+	
+	// Schlagwörter
+    var maxSchlagwoerter = 10; // wie viele Inputs?
+    $('#formUpload')
+	.on('click', '.addButton', function() {
+		var $template = $('#missingTemplate');
+		var $clone = $template
+			.clone()
+			.removeClass('hide')
+			.removeAttr('id')
+			.children().children('input').attr('required', true).parents().parents() // notwenig wegen required
+			.insertBefore($template);
+		var $schlagwort = $clone.find('[name="schlagwort[]"]');
+		if ($('#formUpload').find(':visible[name="schlagwort[]"]').length >= maxSchlagwoerter)
+		{
+			$('#formUpload').find('.addButton').attr('disabled', 'disabled');
+		}
+	})
+	.on('click', '.removeButton', function() {
+		var $row    = $(this).parents('.form-group');
+		var $schlagwort = $row.find('[name="schlagwort[]"]');
+		$row.remove();
+		if ($('#formUpload').find(':visible[name="schlagwort[]"]').length < maxSchlagwoerter)
+		{
+			$('#formUpload').find('.addButton').removeAttr('disabled');
+		}
+	});
 });
 
 // wird beim Wechsel des Fachbereichs Aufgerufen
