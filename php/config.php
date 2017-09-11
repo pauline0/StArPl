26010f6215fb656fb69f8d341502f7e8039f18eb
leo7044 <cc.ta.leo7044@gmail.com>
2017-09-11 22:24:43 +0200

    Datenbank-Konfiguration erstellt

---
+++ php/config.php
@@ -0,0 +1,8 @@
+<?php
+// error_reporting(0);
+$server = "localhost";
+$database = "starpl";
+$username = "starpl";
+$password = "starpl";
+$conn = new mysqli($server, $username, $password, $database);
+?>
